7b1d46f8c58b0cadbcb85e23a34c974ea7040887
Andi Dittrich <andi@andidittrich.de>
2018-08-04 15:36:00 +0200

    added block transformation rules

--- src/blocks/codeblock.js
+++ src/blocks/codeblock.js
@@ -6,7 +6,9 @@
 // Copyright 2018 Andi Dittrich <https://andidittrich.de>
 // ----------------------------------------------------------------------
 
+import _wp from '../wp';
 import _TextEditor from '../components/TextEditor';
+import _Toolbar from '../components/Toolbar';
 
 // Standard Codeblock
 export default {
@@ -25,6 +27,19 @@ export default {
             type: 'string',
             selector: 'pre.EnlighterJSRAW',
             source: 'text'
+        },
+
+        config: {
+            source: 'query',
+            selector: 'pre.EnlighterJSRAW',
+            query: {
+                language:    { source: 'attribute', attribute: 'data-enlighter-language' },
+                theme:       { source: 'attribute', attribute: 'data-enlighter-theme' },
+                highlight:   { source: 'attribute', attribute: 'data-enlighter-highlight' },
+                linenumbers: { source: 'attribute', attribute: 'data-enlighter-linenumbers' },
+                lineoffset:  { source: 'attribute', attribute: 'data-enlighter-lineoffset' },
+                title:       { source: 'attribute', attribute: 'data-enlighter-title' },
+            }
         }
     },
 
@@ -41,9 +56,10 @@ export default {
         html: false
     },
 
-    // allow transform from RAW DOM <pre> node
+   // block element transformation/converting
     transforms: {
         from: [
+             // allow transform from RAW DOM <pre> node
             {
                 type: 'raw',
                 isMatch: ( node ) => (
@@ -57,6 +73,37 @@ export default {
                         },
                     }
                 }
+            },
+
+            // allow transform from core/code block
+            // allow transform from core/preformatted block
+            // allow transform from core/paragraph block
+            {
+                type: 'block',
+                blocks: ['core/code', 'core/preformatted', 'core/paragraph'],
+                transform: function ({content}) {
+                    return _wp.blocks.createBlock('enlighter/codeblock', {content});
+                },
+            }
+        ],
+
+        to: [
+            // allow transform to core/code block
+            {
+                type: 'block',
+                blocks: ['core/code'],
+                transform: function ({content}) {
+                    return _wp.blocks.createBlock('core/code', {content});
+                },
+            },
+
+            // allow transform to core/preformatted block
+            {
+                type: 'block',
+                blocks: ['core/preformatted'],
+                transform: function ({content}) {
+                    return _wp.blocks.createBlock('core/preformatted', {content});
+                },
             }
         ]
     },
@@ -65,9 +112,15 @@ export default {
     edit: function blockEdit({attributes, setAttributes, className}){
 
         // use standard Gutenberg PlainText View with custom styles
-        return (
+        return [
+            <_Toolbar key="controls">
+            
+            <wp.editor.AlignmentToolbar />
+            </_Toolbar>,
+
             // outer container
             <div className={className}>
+                
                 <_TextEditor
                     value={ attributes.content }
                     onChange={(content) => setAttributes({content})}
@@ -76,7 +129,7 @@ export default {
                 />
                 <div className="enlighter-footer-label"><small>EnlighterJS Syntax Highlighter</small></div>
             </div>
-        );
+        ];
     },
 
     // render element as html
@@ -84,8 +137,6 @@ export default {
         console.log(attributes);
         // add enlighterjs related attributes
         return <pre 
-            data-enlighter-language="generic"
-            data-enlighter-theme="ddd"
             className="EnlighterJSRAW"
             >
             {attributes.content}
